318087021131c65417aea237d149ed5f31b81498
Santiago Mok <santiago.mok@gmail.com>
2020-12-30 22:28:06 -0800

    ssh config file

--- macos/install.sh
+++ macos/install.sh
@@ -13,17 +13,19 @@ function _link_to() {
     local link_name
     target=$1
     link_name=$2
-    [[ -h $link_name ]] && rm -vf $link_name && echo "\t link removed ... OK" 
-    ln -svf $target $link_name && echo "\t link ... OK"
+    [[ -h $link_name ]] && rm -vf $link_name && echo -e "  |___ link removed ... OK" 
+    ln -svf $target $link_name && echo -e "  |___ link ... OK"
 }
 
-[[ ! -d $HOME/local/bin ]]          && mkdir -pv "$HOME/local/bin" && echo "mkdir $HOME/local/bin ... OK" 
-[[ ! -d $HOME/local/share/man ]]    && mkdir -pv "$HOME/local/share/man" && echo "mkdir $HOME/local/share/man ... OK"
+[[ ! -d $HOME/local/bin ]]          && mkdir -p "$HOME/local/bin" && echo "mkdir $HOME/local/bin ... OK" 
+[[ ! -d $HOME/local/share/man ]]    && mkdir -p "$HOME/local/share/man" && echo "mkdir $HOME/local/share/man ... OK"
 
 # _link_to $SRC/macos/config       $HOME/.config
 # _link_to $SRC/macos/zshrc        $HOME/.zshrc
 # _link_to $SRC/macos/nvimrc       $HOME/.vimrc
-# _link_to $HOME/.config/vim       $HOME/.vim
 # _link_to $SRC/macos/tmux.conf    $HOME/.tmux.conf
 # _link_to $SRC/../git/gitconfig   $HOME/.gitconfig
 # _link_to $SRC/../git/gitignore   $HOME/.gitignore
+
+# _link_to $HOME/.config/vim       $HOME/.vim
+_link_to $HOME/.config/ssh       $HOME/.ssh
