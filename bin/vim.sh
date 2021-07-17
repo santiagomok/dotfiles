bb661331375a5f25ae21bbbe059ffef3813007fd
Santiago Mok <santiago.mok@gmail.com>
2021-07-17 00:44:51 -0700

    reorg

--- bin/vim.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-install_plugin() {
-    [ ! -d ${HOME}/.vim/bundle ] && mkdir -p ${HOME}/.vim/bundle
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
-    vim +PluginInstall +qall
-}
-
-install_plugin
