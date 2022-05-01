e1aa31b6e386ad05c966d3df47bf96d577a873d4
Santiago Mok <santiago.mok@gmail.com>
2022-05-01 13:34:57 -0700

    wezterm config

---
+++ macos/config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+-- WezTerm
+-- https://wezfurlong.org/wezterm
+
+local wezterm = require 'wezterm'
+
+return {
+    -- Smart tab bar
+    hide_tab_bar_if_only_one_tab = true, 
+
+    -- Color scheme
+    color_scheme = "Seti", 
+    -- color_scheme = 'VibrantInk', 
+    -- color_scheme = 'OneHalfDark', 
+
+    -- Font configuraiton
+    font = wezterm.font("JetBrains Mono", {weight="Bold"}),  -- default
+    -- font = wezterm.font('Source Code Pro', {weight="Bold"}), 
+    font_size = 14.0, 
+
+}
